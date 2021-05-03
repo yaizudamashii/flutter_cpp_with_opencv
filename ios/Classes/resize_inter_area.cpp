ef75862510fe4f485264501b364a97d354485d71
Yuki Konda <yuki.konda@gmail.com>
2021-05-02 20:38:38 -0400

    added resize function

---
+++ ios/Classes/resize_inter_area.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Yuki Konda on 5/2/21.
+//
+
+#include <stdint.h>
+#include <opencv2/core.hpp>
+
+extern "C" __attribute__((visibility("default"))) __attribute__((used))
+uint8_t *resize_inter_area(uint8_t *src, uint8_t *dst, int32_t srcWidth, int32_t srcHeight, int32_t dstWidth, int32_t dstHeight) {
+
+    cv::Mat original(srcWidth, srcHeight, CV_8UC3, src);
+    cv::Mat resized(dstWidth, dstHeight, CV_8UC3);
+
+    cv::resize(original, resized, cv::Size(dstWidth, dstHeight), 0, 0, CV_INTER_AREA);
+    return resized.data;
+}
--- lib/native_with_opencv.dart
+++ lib/native_with_opencv.dart
@@ -6,3 +6,24 @@ final DynamicLibrary nativeAddLib =
 
 final int Function(int x, int y) nativeAdd =
     nativeAddLib.lookup<NativeFunction<Int32 Function(Int32, Int32)>>("native_add").asFunction();
+
+final Pointer<Uint8> Function(Pointer<Uint8> original, int originalWidth, int originalHeight, int resizedWidth, int resizedHeight) resizeInterArea =
+    nativeAddLib.lookup<NativeFunction<Pointer<Uint8> Function(Pointer<Uint8>, Int32, Int32, Int32, Int32)>>("resize_inter_area").asFunction();
+
+Pointer<T> allocate<T extends NativeType>({int count = 1}) {
+    final int totalSize = count * sizeOf<T>();
+    Pointer<T> result;
+    if (Platform.isWindows) {
+        result = winHeapAlloc(processHeap, /*flags=*/ 0, totalSize).cast();
+    } else {
+        result = posixMalloc(totalSize).cast();
+    }
+    if (result.address == 0) {
+        throw ArgumentError("Could not allocate $totalSize bytes.");
+    }
+    return result;
+}
+
+final void free(Pointer ptr) {
+    posixFree(ptr);
+}
