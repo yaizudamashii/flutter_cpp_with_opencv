b6cdb8df31a9fee14af4fd371a9f7c065df44aea
Yuki Konda <yuki.konda@gmail.com>
2021-05-03 13:22:25 -0400

    more debug

--- ios/Classes/resize_inter_area.cpp
+++ ios/Classes/resize_inter_area.cpp
@@ -10,9 +10,6 @@
 extern "C" __attribute__((visibility("default"))) __attribute__((used))
 uint8_t *resize_inter_area(uint8_t *src, int32_t srcWidth, int32_t srcHeight, int32_t dstWidth, int32_t dstHeight) {
 
-    cv::Mat original(srcWidth, srcHeight, CV_8UC3, src);
-    cv::Mat resized(dstWidth, dstHeight, CV_8UC3);
 
-    cv::resize(original, resized, cv::Size(dstWidth, dstHeight), 0, 0, cv::INTER_AREA);
-    return resized.data;
+    return src;
 }
