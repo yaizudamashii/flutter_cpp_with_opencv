c0dc6e2ab630440bacfc305de84239c195614dee
Yuki Konda <yuki.konda@gmail.com>
2021-05-02 21:02:05 -0400

    added missing headers

--- ios/Classes/resize_inter_area.cpp
+++ ios/Classes/resize_inter_area.cpp
@@ -4,6 +4,8 @@
 
 #include <stdint.h>
 #include <opencv2/core.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 extern "C" __attribute__((visibility("default"))) __attribute__((used))
 uint8_t *resize_inter_area(uint8_t *src, uint8_t *dst, int32_t srcWidth, int32_t srcHeight, int32_t dstWidth, int32_t dstHeight) {
